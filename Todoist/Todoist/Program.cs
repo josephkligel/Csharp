d8f694537593615d84e3541eb47d060af8ce84df
Joseph Kligel <jkligel@gmail.com>
2024-08-30 07:50:17 -0500

    Start Ultimate C# course on Udemy

---
+++ Todoist/Todoist/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[Remove] a TODO");
+Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+Console.WriteLine("User input: " + userChoice);
+userChoice = "ABC";
+
+var result = "abc" + "def" + "ghi";
+
+Console.ReadKey();
---
+++ VariableNames/VariableNames/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+string @class = "first";
+
+string aWord = "word";
+
+int number100 = 100;
+
+int count;
+// Microsoft convention is to name variables in lowercase and lower camelcase
+string thisSentenceIsWrittenInLowerCamelCase;
+
+/* Naming variables in C# - tips:
+ * 1. Avoid abbreviations
+ * 2. Be precise with variable naming
+ * 3. Take your time
+ * 4. You can always rename a variable to something better.
