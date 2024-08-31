5e2b882013ca9f0d235e1041d2e82002206e5411
Joseph Kligel <jkligel@gmail.com>
2024-08-30 21:44:17 -0500

    C# methods

--- Todoist/Todoist/Program.cs
+++ Todoist/Todoist/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
 Console.WriteLine("Hello!");
 Console.WriteLine("What do you want to do?");
 Console.WriteLine("[S]ee all TODOs");
@@ -7,13 +9,42 @@ Console.WriteLine("[Remove] a TODO");
 Console.WriteLine("[E]xit");
 
 // reads line from console and assigns it to variable
-string userChoice = Console.ReadLine();
+var userChoice = Console.ReadLine();
+bool isLong = IsLong(userChoice);
 
+bool IsLong(string input)
+{
+    return input.Length > 10;
+}
 
-Console.WriteLine("User input: " + userChoice);
+var result = Add(10, 5);
+Console.WriteLine("10 + 5 = " + result);
 
-bool isUserInputAbc = userChoice == "ABC";
-bool isUserInputNotAbc1 = userChoice != "ABC";
-bool isUserInputNotAbc2 = !(userChoice == "ABC");
+//if(userChoice == "S")
+//{
+//    PrintSelectedOption("See all TODOs");
+//}
+//else if (userChoice == "A")
+//{
+//    PrintSelectedOption("Add a TODO");
+//}
+//else if (userChoice == "R")
+//{
+//    PrintSelectedOption("Remove a TODO");
+//}
+//else if (userChoice == "E")
+//{
+//    PrintSelectedOption("Exit");
+//}
+
+//void PrintSelectedOption(string selectedOption)
+//{
+//    Console.WriteLine("Selected option: " + selectedOption);
+//}
+
+int Add(int a, int b)
+{
+    return a + b;
+}
 
 Console.ReadKey();
