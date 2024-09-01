86b7a6679672ec6896f845cc5a58410d83e0f7b0
Joseph Kligel <jkligel@gmail.com>
2024-09-01 11:09:45 -0500

    Do while loops

--- Todoist/Todoist/Program.cs
+++ Todoist/Todoist/Program.cs
@@ -1,39 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//// See https://aka.ms/new-console-template for more information
 using System.ComponentModel;
 
-//Console.WriteLine("Hello!");
-//Console.WriteLine("What do you want to do?");
-//Console.WriteLine("[S]ee all TODOs");
-//Console.WriteLine("[A]dd a TODO");
-//Console.WriteLine("[Remove] a TODO");
-//Console.WriteLine("[E]xit");
+Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
 
-//var userChoice = Console.ReadLine();
+var userChoice = Console.ReadLine();
 
+switch (userChoice)
+{
+    case "S":
+    case "s":
+        PrintSelectedOption("See all TODOs");
+        break;
+    case "A":
+    case "a":
+        PrintSelectedOption("Add a TODO");
+        break;
+    case "R":
+    case "r":
+        PrintSelectedOption("Remove a TODO");
+        break;
+    case "E":
+    case "e":
+        PrintSelectedOption("Exit");
+        break;
+    default:
+        Console.WriteLine("Invalid choice.");
+        break;
+}
 
-//if(userChoice == "S")
-//{
-//    PrintSelectedOption("See all TODOs");
-//}
-//else if (userChoice == "A")
-//{
-//    PrintSelectedOption("Add a TODO");
-//}
-//else if (userChoice == "R")
-//{
-//    PrintSelectedOption("Remove a TODO");
-//}
-//else if (userChoice == "E")
-//{
-//    PrintSelectedOption("Exit");
-//}
+void PrintSelectedOption(string userChoice)
+{
+    Console.WriteLine($"Selected choice: {userChoice}");
+}
 
-//void PrintSelectedOption(string selectedOption)
-//{
-//    Console.WriteLine("Selected option: " + selectedOption);
-//}
-
-int a = 4, b = 2, c = 10;
-Console.WriteLine($"First is {a}, second is {b}, third is: {c}");
+char ConvertPointsToGrade(int points)
+{
+    switch (points)
+    {
+        case 10:
+        case 9:
+            return 'A';
+        case 8:
+        case 7:
+        case 6:
+            return 'B';
+        case 5:
+        case 4:
+        case 3:
+            return 'C';
+        case 2:
+        case 1:
+            return 'D';
+        case 0:
+            return 'E';
+        default:
+            return '!';
+    }
+}
 
 Console.ReadKey();
--- Todoist/Todoist/obj/Debug/net8.0/Todoist.AssemblyInfo.cs
+++ Todoist/Todoist/obj/Debug/net8.0/Todoist.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todoist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3021c55aeb3f38a8db321240dd1b0e6be85854b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e116402d2313527eff41048d8629a529c75bbd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todoist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todoist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
