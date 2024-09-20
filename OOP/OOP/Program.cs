52b8ba9ea907cf7235b36a70c295e6a9e1173990
Joseph Kligel <jkligel@gmail.com>
2024-09-20 18:27:42 -0500

    Learn polymorphism methods

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,42 @@
+﻿var pizza = new Pizza();
+pizza.AddIngredient(new Cheddar());
+pizza.AddIngredient(new Mozzarella());
+pizza.AddIngredient(new TomatoSauce());
+
+Console.WriteLine(pizza.Describe());
+
+Console.ReadKey();
+
+public class Pizza
+{
+    private List<Ingredient> _ingredients = new();
+
+    public void AddIngredient(Ingredient ingredient) =>
+        _ingredients.Add(ingredient);
+
+    public string Describe() =>
+        $"This is a pizza with {string.Join(", ", _ingredients)}";
+}
+
+public class Ingredient
+{
+
+}
+
+public class Cheddar : Ingredient
+{
+    public string Name => "Cheddar cheese";
+    public int AgedForMonths { get; }
+}
+
+public class TomatoSauce: Ingredient
+{
+    public string Name => "Tomatoe sauce";
+    public int TomatoesIn100Grams { get; }
+}
+
+public class Mozzarella : Ingredient
+{
+    public string Name => "Mozarella";
+    public bool IsLight { get; }
+}
