c9de31a6d5d2304cc93b91ae0b4f9a0661a8463e
Joseph Kligel <jkligel@gmail.com>
2024-12-01 18:11:54 -0600

    Start GameDataParser project

---
+++ GameDataParser/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDataParser
+{
+	public class Game
+	{
+		public string Title { get; set; }
+		public int ReleaseYear { get; set; }
+		public float Rating { get; set; }
+
+		public override string ToString()
+		{
+			return $"{Title}, released in {ReleaseYear}, rating: {Rating}";
+		}
+	}
+
+	
+
+}
---
+++ GameDataParser/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO.Enumeration;
+using System.Text.Json;
+using System.Threading.Channels;
+using System.Xml.Linq;
+using GameDataParser;
+
+var filename = "";
+bool isFileValid = false;
+var fileContents = "";
+do
+{
+    Console.WriteLine("Enter the name of the file you want to read: ");
+
+    try
+    {
+        filename = Console.ReadLine();
+        isFileValid = true;
+        fileContents = File.ReadAllText(filename);
+    }
+    catch (ArgumentNullException ex)
+    {
+        Console.WriteLine("File name cannot be null.");
+    }
+	catch (FileNotFoundException ex)
+	{
+		Console.WriteLine("File not found.");
+	}
+	catch (ArgumentException ex)
+	{
+        Console.WriteLine("File name cannot be empty.");
+    }
+    
+} while (!isFileValid);
+
+List<Game> games = JsonSerializer.Deserialize<List<Game>>(fileContents);
+
+Console.WriteLine("Loaded games are: ");
+
+foreach(var game in games)
+{
+    Console.WriteLine(game);
+}
+
+Console.ReadKey();
