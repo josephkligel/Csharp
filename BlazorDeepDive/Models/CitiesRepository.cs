7aa29c3f654b461e285b5490ad1bbb24202b28ef
Joseph Kligel <jkligel@gmail.com>
2024-12-07 08:57:10 -0600

    Add Blazor Deep Dive project

---
+++ BlazorDeepDive/Models/CitiesRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorDeepDive.Models
+{
+    public class CitiesRepository
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => cities;
+
+    }
+}
