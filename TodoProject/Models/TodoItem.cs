e8b5b09f6b73d5f2953aa520c8170c185215fd63
Joseph Kligel <jkligel@gmail.com>
2024-12-22 14:09:33 -0600

    Finish todo app part 1

---
+++ TodoProject/Models/TodoItem.cs
@@ -0,0 +1,25 @@
+﻿namespace TodoProject.Models
+{
+    public class TodoItem
+    {
+        public int TodoId { get; set; }
+        public string Title { get; set; } = "";
+
+        public bool _isDone;
+
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                _isDone = value;
+
+                if (value)
+                {
+                    CompletedDate = DateTime.Now;
+                }
+            }
+        }
+        public DateTime CompletedDate { get; set; }
+    }
+}
---
+++ TodoProject/Models/TodoItemRepository.cs
@@ -0,0 +1,40 @@
+﻿namespace TodoProject.Models
+{
+	public class TodoItemRepository
+	{
+		public static List<TodoItem> TodoItems { get; set; } = new()
+		{
+			new TodoItem
+			{
+				TodoId = 0,
+				Title = "Something"
+			}
+		};
+
+		public static void AddTodo(TodoItem todoItem)
+		{
+			int maxId = 0;
+			if (TodoItems.Any()) maxId = TodoItems.Max(t => t.TodoId);
+			todoItem.TodoId = maxId + 1;
+			TodoItems.Add(todoItem);
+		}
+
+		public static void UpdateTodo(int todoId, TodoItem todoItem)
+		{
+			if (todoItem.TodoId != todoId) return;
+			var updateTodoItem = TodoItems.FirstOrDefault(t => t.TodoId == todoId);
+			if (updateTodoItem != null)
+			{
+				updateTodoItem.Title = todoItem.Title;
+			}
+		}
+
+		public static void DeleteTodo(TodoItem todoItem)
+		{
+			if(todoItem != null)
+			{
+				TodoItems.Remove(todoItem);
+			}
+		}
+	}
+}
