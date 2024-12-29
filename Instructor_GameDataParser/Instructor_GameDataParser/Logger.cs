3247722d8d37d267d59596c2764c671b51904875
Joseph Kligel <jkligel@gmail.com>
2024-12-28 20:38:32 -0600

    Start instructor version of GameDataParser app

---
+++ Instructor_GameDataParser/Instructor_GameDataParser/Logger.cs
@@ -0,0 +1,21 @@
+﻿internal class Logger
+{
+    private string _logFileName;
+
+    public Logger(string fileName)
+    {
+        _logFileName = fileName;
+    }
+
+    public void Log(Exception ex)
+    {
+        var entry =
+            $@"[{DateTime.Now}]
+Exception message: {ex.Message}
+Stack trace: {ex.StackTrace}
+
+";
+
+        File.AppendAllText(_logFileName, entry);
+    }
+}
