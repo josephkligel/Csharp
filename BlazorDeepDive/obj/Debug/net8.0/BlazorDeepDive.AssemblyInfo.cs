d5094868528d6e14a9223aea05309b583b1fb6a4
Joseph Kligel <jkligel@gmail.com>
2025-01-17 21:10:10 -0600

    Finish lesson on using html table

--- BlazorDeepDive/obj/Debug/net8.0/BlazorDeepDive.AssemblyInfo.cs
+++ BlazorDeepDive/obj/Debug/net8.0/BlazorDeepDive.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorDeepDive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a99192be66b999062f315fb9c6afcb911a3fb1f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dbd26486588755bfffb87903386455057de0024")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorDeepDive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorDeepDive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
