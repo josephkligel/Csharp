4a13567c54efac6ecd27b2589346350195b45d9b
Joseph Kligel <jkligel@gmail.com>
2025-02-01 15:16:21 -0600

    Finished lesson 176

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿var decimals = new List<decimal>() { 1.1m, 0.5m, 22.5m, 12m};
+var ints = decimals.ConvertTo();
+
+Console.ReadKey();
+
+static class ListExtentions
+{
+    public static List<TTarget> ConvertTo<TSource, TTarget>(this List<TSource> decimals)
+    {
+        var result = new List<TTarget>();
+
+        foreach(var item in decimals)
+        {
+            result.Add((typeof(TTarget))item);
+        }
+
+        return result;
+    }
+    public static void AddToFront<T>(this List<T> list, T item)
+    {
+        list.Insert(0, item);
+    }
+}
---
+++ Tuples/Tuples/Program.cs
@@ -0,0 +1,44 @@
+﻿List<int> numbers = new List<int> { 5, 3, 2, 8, 16, 7 };
+SimpleTuple<int, int> minAndMax = GetMinAndMax(numbers);
+Console.WriteLine("Smallest number is " + minAndMax.Item1);
+Console.WriteLine("Largest number is " + minAndMax.Item2);
+
+Console.ReadKey();
+
+SimpleTuple<int, int> GetMinAndMax(IEnumerable<int> input)
+{
+    if (!input.Any())
+    {
+        throw new InvalidOperationException("The input collection cannot be empty");
+    }
+
+    int min = input.First();
+    int max = input.First();
+
+    foreach (int number in numbers)
+    {
+        if (number < min)
+        {
+            min = number;
+        }
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+
+    return new(min, max);
+}
+
+public class SimpleTuple<T1, T2>
+{
+    public SimpleTuple(T1 item1, T2 item2
+        )
+    {
+        Item1 = item1;
+        Item2 = item2;
+    }
+    public T1 Item1 { get; set; }
+
+    public T2 Item2 { get; set; }
+}
