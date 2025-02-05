7c16a3ec136a8da79aa909d17a0263052c994046
Joseph Kligel <jkligel@gmail.com>
2025-02-04 20:19:16 -0600

    Finished lesson 184

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -1,54 +1,118 @@
-﻿var people = new List<Person>()
-{
-    new Person(){Name = "Anna", YearOfBirth = 1815},
-    new Person(){Name = "Bill", YearOfBirth = 2150}
-};
+﻿//var people = new List<Person>()
+//{
+//    new Person(){Name = "Anna", YearOfBirth = 1815},
+//    new Person(){Name = "Bill", YearOfBirth = 2150}
+//};
 
-var employees = new List<Employee>()
-{
-    new Employee(){Name = "John", YearOfBirth = 1980 },
-    new Employee(){Name = "Anna", YearOfBirth = 1815 }
-};
+//var employees = new List<Employee>()
+//{
+//    new Employee(){Name = "John", YearOfBirth = 1980 },
+//    new Employee(){Name = "Anna", YearOfBirth = 1815 }
+//};
 
-var validPeople = GetOnlyValid(people);
+//var validPeople = GetOnlyValid(people);
 
-var validEmployees = GetOnlyValid(employees);
+//var validEmployees = GetOnlyValid(employees);
 
-foreach (var employee in validEmployees)
-{
-    employee.GoToWork();
-}
+//foreach (var employee in validEmployees)
+//{
+//    employee.GoToWork();
+//}
+
+//Console.ReadKey();
+
+//IEnumerable<TPerson> GetOnlyValid<TPerson>(IEnumerable<TPerson> persons)
+//    where TPerson : Person
+//{
+//    var result = new List<TPerson>();
+
+//    foreach (var person in persons)
+//    {
+//        if (person.YearOfBirth > 1900 &&
+//            person.YearOfBirth < DateTime.Now.Year)
+//        {
+//            result.Add(person);
+//        }
+//    }
+
+//    return result;
+//}
+
+//public class Person
+//{
+//    public string? Name { get; set; }
+//    public int YearOfBirth { get; set; }
+
+//}
+
+//public class Employee: Person
+//{
+//    public void GoToWork()
+//    {
+//        Console.WriteLine("Going to work");
+//    }
+//}
+// ----------------------------------------
+//var john = new Person { Name = "John", YearOfBirth = 1980 };
+//var anna = new Person { Name = "Anna", YearOfBirth = 1915 };
+
+//PrintInOrder(10, 5);
+
+//PrintInOrder("fff", "ddd");
+
+//PrintInOrder(john, anna);
+
+//Console.ReadKey();
+
+//void PrintInOrder<T>(T first, T second) where T: IComparable<T>
+//{
+//	if (first.CompareTo(second) > 0)
+//	{
+//        Console.WriteLine($"{second} {first}");
+//	}
+//	else
+//	{
+//		Console.WriteLine($"{first} {second}");
+//	}
+//}
+
+//public class Person : IComparable<Person>
+//{
+//	public string Name { get; init; }
+//	public int YearOfBirth { get; init; }
+
+//    public override string ToString()
+//    {
+//		return $"{Name} born in {YearOfBirth}";
+//    }
+
+//    public int CompareTo(Person other)
+//	{
+//		if (YearOfBirth < other.YearOfBirth)
+//		{
+//			return 1;
+//		}
+//		else if (YearOfBirth > other.YearOfBirth)
+//		{
+//			return -1;
+//		}
+//		else
+//		{
+//			return 0;
+//		}
+//	}
+//}
+// ------------------------------------------
+using System.Numerics;
+
+Console.WriteLine("Square of 2 is: " + Calculator.Square(2));
+Console.WriteLine("Square of 4m is: " + Calculator.Square(4m));
+Console.WriteLine("Square of 6d is: " + Calculator.Square(6d));
 
 Console.ReadKey();
 
-IEnumerable<TPerson> GetOnlyValid<TPerson>(IEnumerable<TPerson> persons)
-    where TPerson : Person
+public static class Calculator
 {
-    var result = new List<TPerson>();
-
-    foreach (var person in persons)
-    {
-        if (person.YearOfBirth > 1900 &&
-            person.YearOfBirth < DateTime.Now.Year)
-        {
-            result.Add(person);
-        }
-    }
-
-    return result;
+	public static T Square<T>(T input) where T: INumber<T> => input * input;
 }
 
-public class Person
-{
-    public string? Name { get; set; }
-    public int YearOfBirth { get; set; }
-
-}
-
-public class Employee: Person
-{
-    public void GoToWork()
-    {
-        Console.WriteLine("Going to work");
-    }
-}
--- Generics/Generics/obj/Debug/net8.0/Generics.AssemblyInfo.cs
+++ Generics/Generics/obj/Debug/net8.0/Generics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Generics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fafc37e46b51d47cced5e0b85f6fb5f19d438d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d172b3e4fb7b2019fe585def0792d14f300f6c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Generics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Generics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
