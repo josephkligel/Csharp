886a98f25c15006fb310366c844048215b64af1a
Joseph Kligel <jkligel@gmail.com>
2025-02-06 20:53:55 -0600

    Finished lesson on dictionaries

---
+++ Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿var countryToCurrencyMapping = new Dictionary<string, string>()
+{
+	["USA"] = "USD",
+	["India"] = "INR",
+	["Spain"] = "EUR"
+};
+//countryToCurrencyMapping.Add("USA", "USD");
+//countryToCurrencyMapping.Add("India", "INR");
+//countryToCurrencyMapping.Add("Spain", "EUR");
+
+if (countryToCurrencyMapping.ContainsKey("Spain"))
+{
+	Console.WriteLine($"Currency in Spain is {countryToCurrencyMapping["Spain"]}");
+}
+
+countryToCurrencyMapping["Poland"] = "PLN";
+
+foreach(var countryCurrencyPair in countryToCurrencyMapping)
+{
+	Console.WriteLine(
+		$"Country: {countryCurrencyPair.Key}, " +
+		$"currency: {countryCurrencyPair.Value}"
+		);
+}
+{
+	Console.WriteLine();
+}
+
+Console.ReadKey();
