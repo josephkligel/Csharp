4e63021055489c667e8a507ee87e32df2ac1892d
Joseph Kligel <jkligel@gmail.com>
2025-10-06 20:44:28 -0500

    With expression and readonly structs

---
+++ ReadonlyStruct/ReadonlyStruct/Program.cs
@@ -0,0 +1,14 @@
+﻿readonly struct Point
+{
+    private readonly int _z;
+    public int X { get; init; }
+    public int Y { get; init; }
+
+    public Point(int y, int x)
+    {
+        Y = y;
+        X = x;
+    }
+
+    public override string ToString() => $"X: {X}, Y: {Y}";
+}
---
+++ WithExpression/WithExpression/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+var point = new Point(10, 20);
+var pointMoved = point with { X = 30 };
+
+Console.ReadKey();
+
+struct Point
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
