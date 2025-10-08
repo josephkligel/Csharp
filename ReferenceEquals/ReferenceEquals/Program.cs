bc4889a58bb673e75b2e93be2e663c8c9f7078a1
Joseph Kligel <jkligel@gmail.com>
2025-10-07 19:44:54 -0500

    Finish reference equals and equals

---
+++ ReferenceEquals/ReferenceEquals/Program.cs
@@ -0,0 +1,30 @@
+﻿var john = new Person("John", 1);
+var sameJohn = john;
+
+Console.WriteLine($"Are references equal? {object.ReferenceEquals(john, sameJohn)}");
+Console.WriteLine(object.ReferenceEquals(null, null));
+
+Console.ReadKey();
+
+readonly struct Point
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Person(string name, int id)
+    {
+        Name = name;
+        Id = id;
+    }
+}
