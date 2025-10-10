88982f482f0fb540a4cb10af2f29a591aeec7608
Joseph Kligel <jkligel@gmail.com>
2025-10-09 21:40:06 -0500

    Overloading

---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,35 @@
+﻿var point1 = new Point(2, 3);
+var point2 = new Point(4, 5);
+var sum = point1 + point2;
+
+class Point: IEquatable<Point> {     
+    public int X { get; }
+    public int Y { get; }
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    
+    public static Point operator +(Point p1, Point p2)
+    {
+        return new Point(p1.X + p2.X, p1.Y + p2.Y);
+    }
+
+    public static bool operator ==(Point p1, Point p2)
+    {
+        return p1.Equals(p2);
+    }
+    
+    public static bool operator !=(Point p1, Point p2)
+    {
+        return !p1.Equals(p2);
+    }
+
+    public bool Equals(Point? other)
+    {
+        return other is Point point 
+            && X == other.X 
+            && Y == other.Y;
+    }
+}
---
+++ OverloadingConversion/OverloadingConversion/Program.cs
@@ -0,0 +1,47 @@
+﻿var tuple = Tuple.Create(1, 2);
+Point point = (Point)tuple;
+
+Console.ReadKey();
+
+readonly struct Point
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    //public static Point operator +(Point p1, Point p2)
+    //{
+    //    return new Point(p1.X + p2.X, p1.Y + p2.Y);
+    //}
+
+    //public static bool operator ==(Point p1, Point p2)
+    //{
+    //    return p1.Equals(p2);
+    //}
+
+    //public static bool operator !=(Point p1, Point p2)
+    //{
+    //    return !p1.Equals(p2);
+    //}
+
+    //public bool Equals(Point? other)
+    //{
+    //    return other is Point point
+    //        && X == other.X
+    //        && Y == other.Y;
+    //}
+
+    //public static implicit operator Point(Tuple<int, int> tuple)
+    //{
+    //    return new Point(tuple.Item1, tuple.Item2);
+    //}
+
+    public static explicit operator Point(Tuple<int, int> tuple)
+    {
+        return new Point(tuple.Item1, tuple.Item2);
+    }
+}
